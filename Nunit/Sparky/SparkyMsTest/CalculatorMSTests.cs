50b4ffb81c2f8067c6413a7b37beb50d6558a4e1
Ricardo Garcia <ricardo.garcia@epicor.com>
2022-05-18 10:13:55 -0500

    Now unit test is not failing anymore

---
+++ Nunit/Sparky/SparkyMsTest/CalculatorMSTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMsTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            
+            //Arrange = Initialization
+            Calculator calc = new();
+            //Act = Invoke the methods needed to obtain an input
+            int result = calc.AddNumbers(10, 20);
+            //Assert = Result expected
+            Assert.AreEqual(30, result);
+        }
+    }
+}
