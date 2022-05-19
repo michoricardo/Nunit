6e1ae61697a18cf68631c750dd182b8f140a4ae3
Ricardo Garcia <ricardo.garcia@epicor.com>
2022-05-19 10:32:15 -0500

    added nunit test

---
+++ Nunit/Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+
+            //Arrange = Initialization
+            Calculator calc = new();
+            //Act = Invoke the methods needed to obtain an input
+            int result = calc.AddNumbers(10, 20);
+            //Assert = Result expected
+            Assert.AreEqual(30, result);
+            
+        }
+    }
+}
